8218366e146ee3f8f5921eb4af36d0d42f20730a
deepak076 <deepakjindal2626@gmail.com>
2024-02-08 05:04:14 +0530

    Remove util/ from version control

--- util/db.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('gChat', 'root', 'dj25082001', {
-    host: 'localhost',
-    dialect: 'mysql',
-});
-
-module.exports = sequelize;
